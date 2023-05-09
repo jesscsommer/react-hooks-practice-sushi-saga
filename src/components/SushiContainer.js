35da985b00281c07984f2f9e33f18a2149b1bf09
Jessica Sommerville <jessica.c.sommerville@gmail.com>
2023-05-09 14:08:42 -0700

    Add more sushi button functionality

--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import MoreButton from "./MoreButton";
 import Sushi from './Sushi'
 
 function SushiContainer({ allSushi }) {
+  //TODO: use a state to control what we're slicing 
+  //TODO: create onClick function for more button to change the state/increment by 4 
+
+  const [start, setStart] = useState(0)
+
+  const handleClick = () => {
+    setStart(start => start + 4)
+  }
+
   return (
     <div className="belt">
-      {allSushi.slice(0, 4).map(sushi => <Sushi key={sushi.id} {...sushi} /> )}
-      <MoreButton />
+      {allSushi.slice(start, start + 4).map(sushi => <Sushi key={sushi.id} {...sushi} /> )}
+      <MoreButton handleClick={handleClick} />
     </div>
   );
 }
